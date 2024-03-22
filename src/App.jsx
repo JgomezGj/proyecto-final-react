d82393036c1e3ca8ca4fd4804b0ded53754749ac
Jesus Maria Gomez Jimenez <jesussmgomez@gmail.com>
2024-03-22 11:45:20 -0500

    Segundo avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css'
-import Layout from './componentes/Layout/index'
-import Productos from './vistas/Productos'
+import { Provider } from 'react-redux';
+import './App.css';
+import Layout from './componentes/Layout/index';
+import store from "./store";
+import Productos from './vistas/Productos';
 
 function App() {
   return (
     <>
+      <Provider store={store}>
       <Layout>
         <Productos/>
       </Layout>
+      </Provider>
     </>
   )
 }
 
-export default App
+export default App;
--- src/vistas/Productos.jsx
+++ src/vistas/Productos.jsx
@@ -1,14 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import carritoSlice from "../slices/carrito";
 import "./styles.css";
 
+const {agregarProducto} = carritoSlice.actions;
+
 const Productos = () => {
-    const productos = useState([]);
+    const dispatch = useDispatch();
+    const productosEnStore = useSelector(state => state.carrito.productos);
+    const [productos, setProductos] = useState([]);
+
+useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+        setProductos(data);
+    });
+}, []);
+
+useEffect(() => {
+    console.log("Productos en store", productosEnStore)
+}, [productosEnStore]);
 
     return <div className="productos-container">
         <h1>Productos</h1>
-        <ul>
-            <li>Producto1</li>
-            <li>Producto2</li>
+        <ul className="lista-container">
+            {productos.map(producto => {
+                return (
+                    <li key={producto.id} className="li-container">
+                        <h1> {producto.title} </h1>
+                        <p> {producto.description} </p>
+                        <img src={producto.image} alt={producto.title} />
+                        <button onClick={(evento) => dispatch(agregarProducto(producto))}>
+                            Agregar al carrito </button>
+                    </li>
+                )
+            })}
         </ul>
     </div>
 }
